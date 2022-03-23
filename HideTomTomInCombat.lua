971a2ebedaf29f0848bb864f2d469d32c9a63cc8
plusmouse <plusmouse@protonmail.com>
2022-03-23 21:02:49 +0000

    Fix error when TomTom is disabled

--- HideTomTomInCombat.lua
+++ HideTomTomInCombat.lua
@@ -3,9 +3,7 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_REGEN_DISABLED")
 frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 frame:SetScript("OnEvent", function(f, eventName)
-  if eventName == "PLAYER_REGEN_ENABLED" and TomTomCrazyArrow then
-    TomTomCrazyArrow:Show()
-  else
-    TomTomCrazyArrow:Hide()
+  if TomTomCrazyArrow then
+    TomTomCrazyArrow:SetShown(eventName == "PLAYER_REGEN_ENABLED")
   end
 end)
