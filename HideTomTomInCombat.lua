536bbc79bff2d366d13f8af83b4fc784b4b97c77
plusmouse <plusmouse@protonmail.com>
2022-03-26 15:46:05 +0000

    Change to "OnUpdate"

--- HideTomTomInCombat.lua
+++ HideTomTomInCombat.lua
@@ -2,8 +2,8 @@ local frame = CreateFrame("Frame")
 
 frame:RegisterEvent("PLAYER_REGEN_DISABLED")
 frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-frame:SetScript("OnEvent", function(f, eventName)
+frame:SetScript("OnUpdate", function(f, eventName)
   if TomTomCrazyArrow then
-    TomTomCrazyArrow:SetShown(eventName == "PLAYER_REGEN_ENABLED")
+    TomTomCrazyArrow:SetShown(not InCombatLockdown())
   end
 end)
